fef2bfae71097ce22ba9d981fb2f01de0eb19409
Salwa ★ <loizoo.dwt@gmail.com>
2024-10-22 17:22:50 +0200

    hehe

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -5,7 +5,14 @@ const lines = document.querySelectorAll('.navbar__line');
 toggle.addEventListener('click', () => {
     menu.classList.toggle('is-active');
     toggle.classList.toggle('is-active');
-    lines.forEach((line, index) => {
+    lines.forEach((line) => {
         line.classList.toggle('is-active');
     });
+
+    // Désactive ou active le défilement
+    if (menu.classList.contains('is-active')) {
+        document.body.classList.add('no-scroll');
+    } else {
+        document.body.classList.remove('no-scroll');
+    }
 });
