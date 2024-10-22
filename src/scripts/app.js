933332614a77b2be26056beaf695214aa8893400
Salwa ˶ᵔ ᵕ ᵔ˶ <loizoo.dwt@gmail.com>
2024-10-22 11:25:42 +0200

    NAV

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,11 @@
-console.info('Hello world');
+const toggle = document.querySelector('.navbar__toggle');
+const menu = document.querySelector('.navbar__menu');
+const lines = document.querySelectorAll('.navbar__line');
+
+toggle.addEventListener('click', () => {
+    menu.classList.toggle('is-active');
+    toggle.classList.toggle('is-active');
+    lines.forEach((line, index) => {
+        line.classList.toggle('is-active');
+    });
+});
