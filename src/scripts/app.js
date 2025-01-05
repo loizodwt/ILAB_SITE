d2293285edc2383afa6612e8eee6424f295410a1
Salwa ˶ᵔ ᵕ ᵔ˶ <loizoo.dwt@gmail.com>
2025-01-05 14:15:47 +0100

    améliorations des decos

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,35 +1,37 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const links = document.querySelectorAll('.navbar__link');
-    const toggle = document.querySelector('.navbar__toggle');
-    const menu = document.querySelector('.navbar__menu');
-    const lines = document.querySelectorAll('.navbar__line'); 
+document.addEventListener("DOMContentLoaded", function () {
+  const links = document.querySelectorAll(".navbar__link");
+  const toggle = document.querySelector(".navbar__toggle");
+  const menu = document.querySelector(".navbar__menu");
+  const lines = document.querySelectorAll(".navbar__line");
 
-    links.forEach(link => {
-        link.addEventListener('click', function (e) {
-            links.forEach(l => l.classList.remove('active'));
-            this.classList.add('active');
+  links.forEach((link) => {
+    link.addEventListener("click", function (e) {
+      links.forEach((l) => l.classList.remove("active"));
+      this.classList.add("active");
 
-            if (menu.classList.contains('is-active')) {
-                menu.classList.remove('is-active');
-                toggle.classList.remove('is-active');
-                lines.forEach(line => line.classList.remove('is-active')); 
-            }
+      if (menu.classList.contains("is-active")) {
+        menu.classList.remove("is-active");
+        toggle.classList.remove("is-active");
+        lines.forEach((line) => line.classList.remove("is-active"));
+      }
 
-            const targetId = this.getAttribute('href');
-            const targetSection = document.querySelector(targetId);
+      const targetId = this.getAttribute("href");
+      const targetSection = document.querySelector(targetId);
 
-            window.scrollTo({
-                top: targetSection.offsetTop,
-                behavior: 'smooth'
-            });
+      window.scrollTo({
+        top: targetSection.offsetTop,
+        behavior: "smooth",
+      });
 
-            e.preventDefault(); 
-        });
+      e.preventDefault();
     });
+  });
 
-    toggle.addEventListener('click', function () {
-        menu.classList.toggle('is-active');
-        this.classList.toggle('is-active'); 
-        lines.forEach(line => line.classList.toggle('is-active')); 
-    });
+  toggle.addEventListener("click", function () {
+    menu.classList.toggle("is-active");
+    this.classList.toggle("is-active");
+    lines.forEach((line) => line.classList.toggle("is-active"));
+  });
+
+  ////gsapppppp
 });
